ddb28dd6aea62a010b2d6a600434d266c2614f88
Vinh Thanh Vu <vtv@vinh.com>
2015-09-03 23:59:46 -0400

    LocationViewController Codes

--- Notify/LocationViewController.swift
+++ Notify/LocationViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class LocationViewController: UIViewController {
+class LocationViewController: UIViewController, CLLocationManagerDelegate {
 
     @IBOutlet weak var address: UILabel!
-    
     @IBOutlet weak var mapView: MKMapView!
     
+    var locationManger: CLLocationManager!
+    var previousAddress: String!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        locationManger = CLLocationManager()
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        locationManger.delegate = self
+        locationManger.requestAlwaysAuthorization()
+        locationManger.requestLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,5 +31,12 @@ class LocationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func locationManger(CLLocationManager, didUpgateLocations: [CLLocation] {
+        let location: CLLocation = locations.first!
+        self.mapView.centerCoordinate = location.coordinate
+        let reg = MKCoordinateRegionMakeWithDistance(location.coordinate, 1500, 1500)
+        self.mapView.setRegion(reg, animated: true)
+        geoCode(location)
+    }
 
 }
