c0a75df5f850d22aa6ec506b9cbf0a035453d425
Vinh Thanh Vu <vtv@vinh.com>
2015-09-03 21:54:58 -0400

    import MapKit and CoreLocation

--- Notify/LocationViewController.swift
+++ Notify/LocationViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class LocationViewController: UIViewController {
 
@@ -17,7 +19,6 @@ class LocationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +26,5 @@ class LocationViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
